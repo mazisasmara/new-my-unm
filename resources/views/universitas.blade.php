c93bd8c8755bb7fd4caf18a757b9bf3ed308b3a7
M. Azis Asmara <muhammadAzis070409@gmail.com>
2026-07-26 22:51:16 +0800

    Lupa edit apa intinya commut aja

--- resources/views/universitas.blade.php
+++ resources/views/universitas.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
-  
+
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 px-3">
     @foreach ($layanans as $item)
     <div class="layanan bg-blue-300 text-black px-3 py-3 w-auto text-center">
       <img src="{{ asset($item['logo']) }}" alt="logo" class="w-30 rounded-xl mx-auto"></img>
-      <h1 class="py-5">{{ $item['namaLayanan'] }}</h1>
+      <h1 class="py-3">{{ $item['nama_layanan'] }}</h1>
       <p>{{ $item['deskripsi'] }}</p>
       <button href="{{ $item['link'] }}" class="bg-emerald-300"><a>Kunjungi <br> {{ $item['link'] }}</br></a></a>
     </div>
